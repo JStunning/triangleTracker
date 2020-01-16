2f562562806fd8a002685cf714caceab4191606a
Andriy Veremyeyev and Jack Dunning <jack@>
2020-01-16 13:23:07 -0800

    Complete the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
     $("#formOne").submit(function(event) {
       event.preventDefault();
-      $("#a").text($("#name").val());
-      $("#b").text($("#food").val());
-      $("#c").text($("input:radio[name=flavor]:checked").val());
-      $("#d").text($("#born").val());
-      $("#e").text($("#color").val());
-      $("#f").text($("#phone").val());             
-      $("#result").show();
-    });
+
+    var s1 = parseInt ($("#side1").val());
+    var s2 = parseInt ($("#side2").val());
+    var s3 = parseInt ($("#side3").val());
+
+    var a1 = "Isosceles";
+    var a2 = "Equiteral";
+    var a3 = "Scalene";
+
+    if (s1+s2<= s3 || s2+s3<=s1 || s1+s3<= s2) {
+      alert("This is NOT a triangle. Please, provide values one more time")
+    } else if (s1 === s2 && s2 === s3 && s1 === s3) {
+      $("#type").text(a2);
+    
+    } else if (s1 === s2 || s2 === s3 || s1 === s3) {
+      $("#type").text(a1);
+    
+    } else {
+      $("#type").text(a3);
+    }
+    
+    $("#result").show();
+    }); 
   });  
